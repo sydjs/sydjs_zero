f9594dddc7c28755afb81e17b4ab1edd94656f43
Craig Sharkie <craig.sharkie@gmail.com>
2016-10-30 06:49:43 +1100

    Applying fully qualified URLs

--- tutorial/scripts/scripts.js
+++ tutorial/scripts/scripts.js
@@ -3,10 +3,11 @@
       "vendor/prismjs/prism.min.js",
     ];
     var head = document.head;
+    var source = "https://sydjs.github.io/sydjs_zero/tutorial/"
 
     for (var resource in resources) {
       var element = document.createElement("script");
-      script.href = resources[resource];
+      script.href = source + resources[resource];
       head.appendChild(script);
     }
 }());
