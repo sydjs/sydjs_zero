64e33db33332acae1e63a980e0e43857dca4dcba
Craig Sharkie <craig.sharkie@gmail.com>
2016-10-30 08:26:09 +1100

    Validating the response from the User against the QUIZ answer

--- tutorial/scripts/tutorial.js
+++ tutorial/scripts/tutorial.js
@@ -1,5 +1,7 @@
 (function(){
   var TTRL = {
+    ANSWER: null,
+    QUESTION: null,
     validate: {
       console: function (event) {
         console.log(TTRL.validate.removeHighlight(event.target.innerHTML));
@@ -8,15 +10,35 @@
         return innerHTML.replace(/<[^>]*>/g, '')
                .replace(/\s{2,}/g, ' ')
                .trim();
-      }
+      },
+      validate: function (answer) {
+        var correct = false;
+
+        if (answer === TTRL.ANSWER) {
+          // turn on buttons
+          console.log("SUCCESS");
+        } else {
+          console.log("Not quite");
+        }
+      },
     },
     listen: function () {
       document.querySelector("#console").addEventListener("blur", TTRL.validate.console);
     },
+    quiz: function (tag) {
+      // receive a ?string or #tag, eventually
+
+      TTRL.QUESTION = QUIZ[tag][0];
+      TTRL.ANSWER = QUIZ[tag][1];
+    },
+    search: function () {
+      // set the question we're at by reading a ? or # value from the URL
+    },
     init: function () {
       function loaded() {
         TTRL.listen();
-        console.timeEnd('loaded()');
+        TTRL.search();
+        TTRL.quiz("q0");
       }
 
       console.time('loaded()');
