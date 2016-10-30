450d977aa81044b19115884d3bf58b9d2d2bc15d
Craig Sharkie <craig.sharkie@gmail.com>
2016-10-30 19:22:13 +1100

    Adding a register of loaded asynchronous methods

--- tutorial/scripts/scripts.js
+++ tutorial/scripts/scripts.js
@@ -5,12 +5,23 @@
       "scripts/quiz.js",
       "scripts/tutorial.js",
     ];
+
     var head = document.head;
-    var source = "https://sydjs.github.io/sydjs_zero/tutorial/"
+    var source = "https://sydjs.github.io/sydjs_zero/tutorial/";
+    var registered = 0;
 
     for (var resource in resources) {
       var element = document.createElement("script");
       element.src = source + resources[resource];
+      element.onload = function(){
+        registered += 1;
+
+        console.log("asyncLoaded += 1 => " + registered);
+
+        if (registered === resources.length) {
+          window.asyncLoaded = true;
+        }
+      };
       head.appendChild(element);
     }
 }());
