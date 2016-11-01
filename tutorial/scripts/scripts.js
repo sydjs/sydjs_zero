0a413467d179e1de8843b1160bea4eb3803960cf
Craig Sharkie <craig.sharkie@gmail.com>
2016-11-02 06:14:22 +1100

    Setting JavaScript file sources based on protocol
    Setting JavaScript file sources based on protocol to load local files
    or remote files depending on current `location`

--- tutorial/scripts/scripts.js
+++ tutorial/scripts/scripts.js
@@ -1,26 +1,45 @@
 (function(){
+    var body = document.body;
     var resources = [
       "vendor/prismjs/prism.min.js",
       "vendor/micro/micro.min.js",
       "scripts/quiz.js",
-      "scripts/tutorial.js",
     ];
 
-    var head = document.head;
-    var source = "https://sydjs.github.io/sydjs_zero/tutorial/";
-    var registered = 0;
+    var initialise = "scripts/tutorial.js";
+
+    var sources = function () {
+      var uris = {
+        file: "file:///Users/HOME/Development/projects/sydjs_zero/tutorial/",
+        http: "https://sydjs.github.io/sydjs_zero/tutorial/"
+      }
+
+      return uris[window.location.protocol.substring(0, 4)];
+    }
+
     var loaded = function () {
+      window.asyncLoaded = true;
+      append(initialise);
+    };
+
+    var registered = 0;
+    var register = function () {
       registered += 1;
 
       if (registered === resources.length) {
-        window.asyncLoaded = true;
+        loaded();
       }
     };
 
-    for (var resource in resources) {
+    var source = sources();
+    var append = function (src) {
       var element = document.createElement("script");
-      element.src = source + resources[resource];
-      element.onload = loaded;
-      head.appendChild(element);
+      element.src = source + src;
+      element.onload = register;
+      body.appendChild(element);
+    }
+
+    for (var resource in resources) {
+      append(resources[resource]);
     }
 }());
